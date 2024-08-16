1dea4b55aa688260d7a658fd96df6c3e354c2495
José Victor <160529147+jzalves@users.noreply.github.com>
2024-08-15 22:22:34 -0300

    final 2

--- rolaetinha.js
+++ rolaetinha.js
@@ -13,7 +13,15 @@ const frases = [
 	"Ão ão ão meu pau na sua mão",
 	"macaco",
 	"Quem clickou é gay",
-	"A ideia é nunca perder a motivação para subir cada degrau rumo ao sucesso"
+	"A ideia é nunca perder a motivação para subir cada degrau rumo ao sucesso",
+	"Agaragã comi tua mãe e tua irmã",
+	"O único lugar onde o sucesso vem antes do trabalho é no dicionário",
+	"Pedro gay",
+	"Matheus gay",
+	"comi a Helena ontem",
+	"A maior glória em viver não está em nunca cair, mas em nos levantar a cada queda",
+	"A vida é 10% o que acontece com você e 90% como você reage a isso."
+
 ];
 
 function mostrarFrase() {
